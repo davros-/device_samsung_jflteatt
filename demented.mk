be42f6a076075a2d5f7179838d2224b74478d00c
cphelps76 <cphelps76@gmail.com>
2013-06-08 20:34:57 -0500

    align with DEMENTED source

--- demented.mk
+++ demented.mk
@@ -1,16 +1,11 @@
 $(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common DEMENTED makefiles.
+$(call inherit-product, vendor/DEMENTED/config/common_gsm.mk)
+$(call inherit-product, vendor/DEMENTED/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys"
 
-PRODUCT_NAME := cm_jflteatt
+PRODUCT_NAME := demented_jflteatt
 PRODUCT_DEVICE := jflteatt
 
