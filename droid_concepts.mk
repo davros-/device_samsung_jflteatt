626c0e0c063cee5eaa42b2e2b4e3e564b2c77e35
davros <davros@droidconcepts.com>
2013-06-20 10:34:36 -0400

    yepper

--- droid_concepts.mk
+++ droid_concepts.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
 
 # Inherit common DEMENTED makefiles.
-$(call inherit-product, vendor/DEMENTED/config/common_gsm.mk)
-$(call inherit-product, vendor/DEMENTED/config/common_phone.mk)
+$(call inherit-product, vendor/Droid_Concepts/config/common_gsm.mk)
+$(call inherit-product, vendor/Droid_Concepts/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys"
 
-PRODUCT_NAME := demented_jflteatt
+PRODUCT_NAME := droid_concepts_jflteatt
 PRODUCT_DEVICE := jflteatt
 
